5d2702c6aa01657fb58ae4039a4282406e6a35aa
Narasak Mansurang <narasak.man@gmail.com>
2020-12-29 19:45:04 +0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1127,10 +1127,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -4
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0.15
-#define X_MAX_POS 255
-#define Y_MAX_POS 212.5
+#define X_MAX_POS 250
+#define Y_MAX_POS 210
 #define Z_MAX_POS 210
 
 /**
